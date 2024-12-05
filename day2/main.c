6e8ac0e6104242a75c3b049d14bee8de451aa416
Sneak Honk <goose@sneakhonk.com>
2024-12-04 20:22:26 -0600

    Day 2 second star

--- day2/main.c
+++ day2/main.c
@@ -15,15 +15,65 @@ size_t offset_incr(uint8_t num) {
 }
 
 uint8_t scan_number(const char* data, size_t offset) {
-    if (data[offset] < '0' || data[offset] > '9')
-    {
-        return UINT8_MAX;
-    }
     if (data[offset + 1] >= '0' && data[offset + 1] <= '9') {
         return (uint8_t)((data[offset] - '0') * 10 + (data[offset + 1] - '0'));
     }
     return (uint8_t)(data[offset] - '0');
 }
+
+uint8_t scan_with_dampener(const char* data, size_t const offset, size_t count) {
+    uint8_t nums[256];
+    size_t num_read = 0;
+    size_t currentoffset = offset;
+
+    while (num_read < count) {
+        nums[num_read] = scan_number(data, currentoffset);
+        currentoffset += offset_incr(nums[num_read]) + 1;
+        num_read++;
+    }
+
+    for (size_t i = 0; i < count; ++i) {
+        uint8_t lastnum = 0, nextnum = 0;
+        int safe = 0;
+        int first = 1;
+
+        for (size_t j = 0; j < count; ++j) {
+            if (j == i) continue;
+
+            if (first) {
+                lastnum = nums[j];
+                first = 0;
+                continue;
+            }
+
+            nextnum = nums[j];
+
+            if (safe == 0) {
+                if (lastnum > nextnum && ((lastnum - nextnum) <= 3)) safe = -1;
+                else if (nextnum > lastnum && ((nextnum - lastnum) <= 3)) safe = 1;
+                else {
+                    safe = 0;
+                    break;
+                }
+            } else {
+                if ((safe == -1) && lastnum > nextnum && ((lastnum - nextnum) <= 3)) {
+                } else if ((safe == 1) && nextnum > lastnum && ((nextnum - lastnum) <= 3)) {
+                } else {
+                    safe = 0;
+                    break;
+                }
+            }
+            lastnum = nextnum;
+        }
+
+        if (safe != 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 
 int main() {
     int fd;
@@ -35,6 +85,9 @@ int main() {
     uint8_t lastnum;
     uint8_t nextnum;
     size_t safe_count;
+    size_t dampener_count;
+    size_t last_report_offset;
+    size_t last_report_length;
 
     fd = open("input", O_RDONLY);
     if (fd == -1) {
@@ -54,6 +107,8 @@ int main() {
     }
     
     offset = 0;
+    last_report_offset = 0;
+    last_report_length = 2;
 
     lastnum = scan_number(data, offset);
     offset += offset_incr(lastnum) + 1;
@@ -65,12 +120,19 @@ int main() {
     else safe = 0;
 
     safe_count = 0;
+    dampener_count = 0;
 
     while (offset < filesize) {
 
         if (data[offset] == '\n') {
             ++offset;
-            if (safe != 0) ++safe_count;
+            if (safe != 0) 
+            {
+                ++safe_count;
+            }
+            else if (scan_with_dampener(data, last_report_offset, last_report_length) != 0) ++dampener_count;
+            last_report_offset = offset;
+            last_report_length = 2;
             lastnum = scan_number(data, offset);
             offset += offset_incr(lastnum) + 1;
             nextnum = scan_number(data, offset);
@@ -80,15 +142,15 @@ int main() {
             else if (nextnum > lastnum && ((nextnum - lastnum) <= 3)) safe = 1;
             else safe = 0;
         }
-
-        if (safe == 0 || data[offset] == ' ')
+        
+        if (data[offset] == ' ') 
         {
             ++offset;
-            continue;
         }
 
         lastnum = nextnum;
         nextnum = scan_number(data, offset);
+        ++last_report_length;
         offset += offset_incr(nextnum);
 
         if ((safe == -1) && lastnum > nextnum && ((lastnum - nextnum) <= 3)) continue;
@@ -98,6 +160,7 @@ int main() {
     }
 
     printf("First star: %zu\n", safe_count);
+    printf("Second star: %zu\n", safe_count + dampener_count);
 
     munmap(data, filesize);
     close(fd);
